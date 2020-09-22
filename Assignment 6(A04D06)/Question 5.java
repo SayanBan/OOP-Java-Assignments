648caf3996efe69abfce42a877817e7db92c1ab4
Sayan bandyopadhyay <45221397+SayanBan@users.noreply.github.com>
2020-09-22 15:09:56 +0530

    Create Question 5.java

---
+++ Assignment 6(A04D06)/Question 5.java
@@ -0,0 +1,43 @@
+abstract class Sh
+{
+    abstract public void area();
+}
+class Cir extends Sh
+{
+    int r;
+    double a;
+    Cir()
+    {
+        r=7;
+    }
+    public void area()
+    {
+        a=(4.0*3.14)*(r*r);
+        System.out.println("Area of circle : "+a);
+    }
+}
+class Rect extends Sh
+{
+    int l,b;
+    double a;
+    Rect()
+    {
+        l=7;
+        b=17;
+    }
+    public void area()
+    {
+        a=l*b;
+        System.out.println("Area of rectangle : "+a);
+    }
+}
+public class Caller5
+{
+    public static void main(String args[])
+    {
+        Sh ob=new Cir();
+        ob.area();
+        Sh ob1=new Rect();
+        ob1.area();
+    }    
+}
