5201939c72d19f7b0954b2ce8406f693a839ef80
Sayan bandyopadhyay <45221397+SayanBan@users.noreply.github.com>
2020-11-03 14:33:30 +0530

    Update and rename Q1.java to Q1 Above diagonal.java

---
+++ A05 Practice 3/Q1 Above diagonal.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+ 
+public class SumOfElementsAboveDiagonal {
+ 
+    public static void main(String...args) {
+           
+           Scanner scanner = new Scanner(System.in);
+           System.out.print("Enter number of rows/columns in matrix : ");    //rows and columns in matrix must be same.
+           int rows = scanner.nextInt();
+           int columns=rows;
+           int[][] matrix = new int[rows][rows];
+           
+           System.out.println("Enter the elements in matrix :");
+           for (int i = 0; i < rows; i++) {
+                  for (int j = 0; j < columns; j++) {
+                        matrix[i][j] = scanner.nextInt();
+                  }
+           }
+ 
+           //Logic to calculate sum of elements above diagonal.
+           int sum=0;
+           for (int j = 1; j < columns; j++) {
+                  for (int i=j-1 ; i>=0 ; i--) {
+                        sum= sum + matrix[i][j];
+                  }
+                  
+           }
+ 
+           System.out.println("\nMatrix is : ");
+           for (int i = 0; i < rows; i++) {
+                  for (int j = 0; j < columns; j++) {
+                        System.out.print(matrix[i][j] + " ");
+                  }
+                  System.out.println();
+           }
+ 
+           System.out.println("sum of elements above diagonal is: "+sum);
+ 
+    }
+}
